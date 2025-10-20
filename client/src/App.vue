92d3b6e9e5cb41d07cbdb2407be18b5b9ab3d209
MichaelSchauer <162590683+MichaelS114@users.noreply.github.com>
2025-10-20 14:37:10 +0200

    Added starter for Vue Frontend

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,47 +1,26 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import ButtonTest from './components/ButtonTest.vue'
+import Logo from './assets/icon.svg'
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div>
+    <img
+      :src="Logo"
+      alt="Music Logo"
+      class="logo h-32 mb-4"
+    />
+    <p class="text-lg font-semibold text-gray-700">Thema 3</p>
+     <div class="mt-8">
+      <ButtonTest />
     </div>
-  </header>
+  </div>
 
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
+/* .logo {
+  filter: drop-shadow(0 0 0.5rem rgba(0, 0, 0, 0.2));
+} */
 </style>
